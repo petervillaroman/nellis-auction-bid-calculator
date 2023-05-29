922881f98c7d71180d83e5219ca9227e80bcdd6d
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-05-29 13:48:55 -0700

    Created logic and css
    converted my python logic into react. used vanilla css to style it simply. Also launched app into github pages.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [bidPrice, setBidPrice] = useState('');
+  const [finalPrice, setFinalPrice] = useState('');
+
+  const calculatePrice = () => {
+    if (isNaN(bidPrice) || bidPrice === '') {
+      alert(" Oops! That was not a valid number. Try again...");
+    } else {
+      let price = parseInt(bidPrice);
+      price = price + (price * 0.15);
+      let finalPrice = price + (price * 0.0838);
+      setFinalPrice(finalPrice.toFixed(2));
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>Nellis Auction Bid Price Calculator</h1>
+        <input type="text" placeholder="Enter your bid price" value={bidPrice} onChange={e => setBidPrice(e.target.value)} />
+        <button onClick={calculatePrice}>Calculate</button>
+        <p>Final Price: {finalPrice}</p>
+      </div>
     </div>
   );
 }
