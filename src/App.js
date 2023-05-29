007d00020a34c80669d1add1f942e08c1645b9e4
Karell Peter <53671374+petervillaroman@users.noreply.github.com>
2023-05-29 13:53:27 -0700

    added variable tax rate for different locations
    first we updated Nevada's tax rate. Then we stored tax rates for all other nellis auction branches.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const taxRates = {
+  "Arizona": 0.056,
+  "Texas": 0.0625,
+  "Nevada": 0.0823
+};
+
 function App() {
   const [bidPrice, setBidPrice] = useState('');
   const [finalPrice, setFinalPrice] = useState('');
+  const [state, setState] = useState('Nevada');
 
   const calculatePrice = () => {
     if (isNaN(bidPrice) || bidPrice === '') {
@@ -11,7 +18,7 @@ function App() {
     } else {
       let price = parseInt(bidPrice);
       price = price + (price * 0.15);
-      let finalPrice = price + (price * 0.0838);
+      let finalPrice = price + (price * taxRates[state]);
       setFinalPrice(finalPrice.toFixed(2));
     }
   }
@@ -19,7 +26,12 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1>Nellis Auction Bid Price Calculator</h1>
+        <h1>Bid Price Calculator</h1>
+        <select value={state} onChange={e => setState(e.target.value)}>
+          {Object.keys(taxRates).map((state) => (
+            <option key={state} value={state}>{state}</option>
+          ))}
+        </select>
         <input type="text" placeholder="Enter your bid price" value={bidPrice} onChange={e => setBidPrice(e.target.value)} />
         <button onClick={calculatePrice}>Calculate</button>
         <p>Final Price: {finalPrice}</p>
